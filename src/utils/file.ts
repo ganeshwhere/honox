fb88b51978ce1471379e8272cdc554a58cc57a9c
GANESH W <155592898+ganeshwhere@users.noreply.github.com>
2024-03-07 23:45:38 +0530

    Update file.ts

--- src/utils/file.ts
+++ src/utils/file.ts
@@ -5,9 +5,9 @@ export const filePathToPath = (filePath: string) => {
     .replace(/^\/?index$/, '/') // `/index`
     .replace(/\/index$/, '') // `/about/index`
     .replace(/\[\.{3}.+\]/, '*')
-    .replace(/\[(.+?)\]/g, ':$1')
-  return /^\//.test(filePath) ? filePath : '/' + filePath
-}
+    .replace(/\[(.+?)\]/g, ':$1');
+  return /^\//.test(filePath) ? filePath : '/' + filePath;
+};
 
 /*
   /app/routes/_error.tsx
@@ -20,56 +20,70 @@ export const filePathToPath = (filePath: string) => {
     ...
   }
  */
+
 export const groupByDirectory = <T = unknown>(files: Record<string, T>) => {
-  const organizedFiles = {} as Record<string, Record<string, T>>
+  const organizedFiles = {} as Record<string, Record<string, T>>;
 
   for (const [path, content] of Object.entries(files)) {
-    const pathParts = path.split('/')
-    const fileName = pathParts.pop()
-    const directory = pathParts.join('/')
+    const pathParts = path.split('/');
+    const fileName = pathParts.pop();
+    const directory = pathParts.join('/');
 
     if (!organizedFiles[directory]) {
-      organizedFiles[directory] = {}
+      organizedFiles[directory] = {};
     }
 
     if (fileName) {
-      organizedFiles[directory][fileName] = content
+      organizedFiles[directory][fileName] = content;
     }
   }
 
-  // Sort the files in each directory
   for (const [directory, files] of Object.entries(organizedFiles)) {
     const sortedEntries = Object.entries(files).sort(([keyA], [keyB]) => {
       if (keyA[0] === '[' && keyB[0] !== '[') {
-        return 1
+        return 1;
       }
       if (keyA[0] !== '[' && keyB[0] === '[') {
-        return -1
+        return -1;
       }
-      return keyA.localeCompare(keyB)
-    })
+      return keyA.localeCompare(keyB);
+    });
 
-    organizedFiles[directory] = Object.fromEntries(sortedEntries)
+    organizedFiles[directory] = Object.fromEntries(sortedEntries);
   }
 
-  return organizedFiles
-}
+  return organizedFiles;
+};
 
+// Sort the files in each directory
 export const sortDirectoriesByDepth = <T>(directories: Record<string, T>) => {
   const sortedKeys = Object.keys(directories).sort((a, b) => {
-    const depthA = a.split('/').length
-    const depthB = b.split('/').length
-    return depthB - depthA
-  })
+    const depthA = a.split('/').length;
+    const depthB = b.split('/').length;
+    return depthB - depthA;
+  });
 
-  const sortedDirectories: Record<string, T>[] = sortedKeys.map((key) => {
-    return { [key]: directories[key] }
-  })
+  return sortedKeys.map((key) => ({ [key]: directories[key] }));
+};
 
-  return sortedDirectories
-}
+export const listByDirectory = <T = unknown>(files: Record<string, T>) => {
+  const organizedFiles = {} as Record<string, string[]>;
 
-/*
+  for (const path of Object.keys(files)) {
+    const pathParts = path.split('/');
+    pathParts.pop();
+    const directory = pathParts.join('/');
+
+    if (!organizedFiles[directory]) {
+      organizedFiles[directory] = [];
+    }
+    if (!organizedFiles[directory].includes(path)) {
+      organizedFiles[directory].push(path);
+    }
+  }
+
+
+  /*
     /app/routes/_renderer.tsx
     /app/routes/blog/_renderer.tsx
     => {
@@ -77,36 +91,22 @@ export const sortDirectoriesByDepth = <T>(directories: Record<string, T>) => {
       '/app/routes/blog': ['/app/routes/blog/_renderer.tsx', '/app/routes/_.tsx']
     }
    */
-export const listByDirectory = <T = unknown>(files: Record<string, T>) => {
-  const organizedFiles = {} as Record<string, string[]>
 
-  for (const path of Object.keys(files)) {
-    const pathParts = path.split('/')
-    pathParts.pop() // extract file
-    const directory = pathParts.join('/')
-
-    if (!organizedFiles[directory]) {
-      organizedFiles[directory] = []
-    }
-    if (!organizedFiles[directory].includes(path)) {
-      organizedFiles[directory].push(path)
-    }
-  }
-
-  const directories = Object.keys(organizedFiles).sort((a, b) => b.length - a.length)
+  
+  const directories = Object.keys(organizedFiles).sort((a, b) => b.length - a.length);
   for (const dir of directories) {
     for (const subDir of directories) {
       if (subDir.startsWith(dir) && subDir !== dir) {
-        const uniqueFiles = new Set([...organizedFiles[subDir], ...organizedFiles[dir]])
-        organizedFiles[subDir] = [...uniqueFiles]
+        const uniqueFiles = new Set([...organizedFiles[subDir], ...organizedFiles[dir]]);
+        organizedFiles[subDir] = [...uniqueFiles];
       }
     }
   }
 
-  return organizedFiles
-}
+  return organizedFiles;
+};
 
 export const pathToDirectoryPath = (path: string) => {
-  const dirPath = path.replace(/[^\/]+$/, '')
-  return dirPath
-}
+  const dirPath = path.replace(/[^\/]+$/, '');
+  return dirPath;
+};
